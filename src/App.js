883d81442092194b18bb9ce173e3462f84e717ff
unknown <Emmanuel Ayodele>
2022-02-20 23:39:10 +0100

    Added icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Swatch from './components/Swatch/swatch'
 function App() {
   return (
     <div className="App">
+      <span>Writeboard</span>
       <Swatch/>
     </div>
   );
--- src/components/Swatch/swatch.js
+++ src/components/Swatch/swatch.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import style from './swatch.module.css';
 
+import pencil from '../../assets/pencil.png';
+import line from '../../assets/line.png';
+import rectangle from '../../assets/rectangle.png';
+import ellipse from '../../assets/ellipse.png';
+import polygon from '../../assets/polygon.png';
+import paint from '../../assets/paint.png';
+import history from '../../assets/history.png';
+import eraser from '../../assets/eraser.png';
+
+
 const Swatch = props => {
     return (
         <div className={style.swatch}>
-            <span>swatch</span>
-            <span>swatch</span>
-            <span>swatch</span>
-            <span>swatch</span>
-            <span>swatch</span>
-            <span>swatch</span>
-
+            <img src={pencil} alt="pencil" className={style.icon} />
+            <img src={line} alt="line" className={style.icon} />
+            <img src={rectangle} alt="rectangle" className={style.icon} />
+            <img src={ellipse} alt="ellipse" className={style.icon} />
+            <img src={polygon} alt="polygon" className={style.icon} />
+            <img src={paint} alt="paint" className={style.icon} />
+            <img src={history} alt="history" className={style.icon} />
+            <img src={eraser} alt="eraser" className={style.icon} />
         </div>
     )
 }
