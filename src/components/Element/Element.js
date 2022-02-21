0fb64e950614891c6edafe86469fb0da6fd499f7
unknown <Emmanuel Ayodele>
2022-02-21 01:16:34 +0100

    Added cursorForPosition function

--- src/components/Element/Element.js
+++ src/components/Element/Element.js
@@ -115,3 +115,20 @@ export const adjustElementCoordinates = element => {
         }
     }
 }
+
+export const cursorForPosition = position => {
+    switch (position) {
+        case 'tl':
+        case 'br':
+        case 'start':
+        case 'end':
+            return 'nwse-resize';
+        case 'tr':
+        case 'bl':
+            return 'nesw-resize';
+        default:
+            return 'move'
+    }
+}
+
+
