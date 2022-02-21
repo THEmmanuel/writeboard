cae2b9321e94424a8522b0fe1e3aec6a018cf1a6
unknown <Emmanuel Ayodele>
2022-02-21 10:29:57 +0100

    set up App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Swatch from './components/Swatch/swatch';
 import rough from 'roughjs/bundled/rough.esm';
@@ -15,12 +15,107 @@ import {
 function App() {
 
   const [points, setPoints] = useState([]);
-  
+  const [path, setPath] = useState([]);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [elements, setElements] = useState([]);
+  const [action, setAction] = useState('none');
+  const [toolType, setToolType] = useState('pencil');
+  const [selectedElement, setSelectedElement] = useState(null);
+  const [colorWidth, setColorWidth] = useState({
+    hex: '#000',
+    hsv: {},
+    rgb: {},
+  });
+  const [width, setWidth] = useState(1);
+  const [shapeWidth, setShapeWidth] = useState(1);
+  const [popped, setPopped] = useState(false);
+
+  useEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    context.lineCap = "round";
+    context.lineJoin = "round";
+
+    context.save();
+
+    const drawpath = () => {
+      path.forEach((stroke, index) => {
+        context.beginPath();
+
+        stroke.forEach((point, i) => {
+          context.strokeStyle = point.newColour;
+          context.lineWidth = point.newLinewidth;
+
+          let midPoint = midPointBetween(point.clientX, point.clientY);
+
+          context.quadraticCurveTo(
+            point.clientX,
+            point.clientY,
+            midPoint.x,
+            midPoint.y
+          );
+          context.lineTo(point.clientX, point.clientY);
+          context.stroke();
+        });
+        context.closePath();
+        context.save();
+      });
+    };
+
+    if (toolType === "eraser" && popped === true) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      setPopped(false);
+    }
+
+    const roughCanvas = rough.canvas(canvas);
+
+    if (path !== undefined) drawpath();
+
+    context.lineWidth = shapeWidth;
+
+    elements.forEach(({ roughElement }) => {
+      context.globalAlpha = "1";
+      //console.log(roughElement);
+      context.strokeStyle = roughElement.options.stroke;
+      roughCanvas.draw(roughElement);
+    });
+
+    return () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [popped, elements, path, width]);
+
+  const updateElement = (
+    index,
+    x1,
+    y1,
+    x2,
+    y2,
+    toolType,
+    strokeWidth,
+    strokeColor
+  ) => {
+    const updatedElement = createElement(
+      index,
+      x1,
+      y1,
+      x2,
+      y2,
+      toolType,
+      strokeWidth,
+      strokeColor
+    );
+
+    const elementsCopy = [...elements];
+    elementsCopy[index] = updatedElement;
+    setElements(elementsCopy);
+  };
 
   return (
     <div className="App">
       <span>Writeboard</span>
-      <Swatch/>
+      <Swatch />
 
 
     </div>
