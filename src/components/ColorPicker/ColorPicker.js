cde3bdf8cac5366ff19776e6b527efeb2fc8292e
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-02-23 11:18:34 +0100

    set up useEffect

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -2,6 +2,9 @@ import React, {useEffect} from 'react';
 import { ColorPicker, useColor } from 'react-color-palette';
 import 'react-color-palette/lib/css/styles.css';
 
-const colorPicker = ({setColorWidth}) => {
+const ColourPicker = ({setColorWidth}) => {
     const [color, setColor] = useColor('hex', '#121212');
+    useEffect(() => {
+        setColorWidth(color);
+    }, [color]);
 }
