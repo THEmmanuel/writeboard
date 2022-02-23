de0908f8ccece38f20224a756f8530b3ec8e452e
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-02-23 21:01:32 +0100

    Add rectangle svg

--- src/assets/svgIcons.js
+++ src/assets/svgIcons.js
@@ -34,33 +34,45 @@ export const Pencil = ({ toolType, colorWidth }) => {
 
 
 export const Line = ({ toolType, colorWidth }) => {
-
-    <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <line x1="1.29289" y1="18.9706" x2="18.9706" y2="1.2929" stroke="black" stroke-width="2" />
-    </svg>
+    return (
+        <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <line x1="1.29289" y1="18.9706" x2="18.9706" y2="1.2929" stroke="black" stroke-width="2" />
+        </svg>
+    )
 }
 
 export const Rectangle = ({ toolType, colorWidth }) => {
-
+    return (
+        <svg
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect x="1" y="1" width="23" height="23" rx="3" stroke="black" stroke-width="2" />
+        </svg>
+    )
 }
 
 export const ellipse = ({ toolType, colorWidth }) => {
-    <svg
-        width="27"
-        height="27"
-        viewBox="0 0 27 27"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <circle cx="13.5" cy="13.5" r="12.5" stroke="black" stroke-width="2" />
-    </svg>
-
+    return (
+        <svg
+            width="27"
+            height="27"
+            viewBox="0 0 27 27"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <circle cx="13.5" cy="13.5" r="12.5" stroke="black" stroke-width="2" />
+        </svg>
+    )
 }
 
 export const polygon = ({ toolType, colorWidth }) => {
