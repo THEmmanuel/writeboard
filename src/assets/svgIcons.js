1868a7e5e7e5d47c5b05e855ab15b70437c28157
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-02-23 19:45:55 +0100

    Added svgIcons for SVG components

---
+++ src/assets/svgIcons.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export const Resize = ({ toolType, colorWidth }) => {
+    return (
+        <svg>
+            <path
+
+            />
+        </svg>
+    )
+}
+
+export const Pencil = ({ toolType, colorWidth }) => {
+
+}
+
+export const Line = ({ toolType, colorWidth }) => {
+
+}
+
+export const Rectangle = ({ toolType, colorWidth }) => {
+
+}
+
+export const ellipse = ({ toolType, colorWidth }) => {
+
+}
+
+export const polygon = ({ toolType, colorWidth }) => {
+
+}
+
+export const paint = ({ toolType, colorWidth }) => {
+
+}
+
+export const history = ({ toolType, colorWidth }) => {
+
+}
+
+export const eraser = ({ toolType, colorWidth }) => {
+
+}
+
