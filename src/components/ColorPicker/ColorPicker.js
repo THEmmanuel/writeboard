1a465ab0ab5d4bd81b0098157253759c3a87ee22
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-02-23 14:17:27 +0100

    set up color picker

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,10 +1,23 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { ColorPicker, useColor } from 'react-color-palette';
 import 'react-color-palette/lib/css/styles.css';
 
-const ColourPicker = ({setColorWidth}) => {
+const ColourPicker = ({ setColorWidth }) => {
     const [color, setColor] = useColor('hex', '#121212');
     useEffect(() => {
         setColorWidth(color);
     }, [color]);
-}
+
+    return (
+        <ColorPicker
+            width={window.innerWidth * 0.1}
+            height={window.innerHeight * 0.1}
+            color={color}
+            onChange={setColor}
+            hideHEX
+            dark
+        />
+    );
+};
+
+export default ColourPicker;
