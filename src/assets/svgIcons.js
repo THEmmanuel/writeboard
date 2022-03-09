aac9af39dcd76ad6312b6aa13a015e892719ef25
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-10 00:08:15 +0100

    added move icon

--- src/assets/svgIcons.js
+++ src/assets/svgIcons.js
@@ -2,18 +2,16 @@ import React from 'react';
 
 export const Move = ({ toolType, colorWidth }) => {
     return (
-        // <svg
-        //     width="26"
-        //     height="26"
-        //     viewBox="0 0 26 26"
-        //     fill="none"
-        //     xmlns="http://www.w3.org/2000/svg"
-        // >
-
-        //     <path d="M3.41625 18.06L18.7125 2.7625C19.3168 2.18421 20.1235 1.86551 20.9599 1.87465C21.7963 1.88379 22.5958 2.22004 23.1874 2.8114C23.7789 3.40277 24.1154 4.20223 24.1247 5.0386C24.1341 5.87498 23.8156 6.68178 23.2375 7.28625L7.93875 22.5837C7.58975 22.9328 7.14524 23.1707 6.66125 23.2675L1.75 24.25L2.7325 19.3375C2.82931 18.8535 3.06722 18.409 3.41625 18.06Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        //     <path d="M16.125 6.125L19.875 9.875" stroke="black" stroke-width="2" />
-        // </svg>
-        'test'
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            width="1em"
+            height="1em"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 16 16">
+            <path fill="currentColor" fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z" />
+        </svg>
     )
 }
 
